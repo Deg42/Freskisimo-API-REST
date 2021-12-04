fa40187a9e05fb3e2ac30040fa79dce4c47de09a
Juan Antonio González <jgonzalezrivera@eci.safanet.es>
2021-12-04 16:00:38 +0100

    Added env cors and entry point

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 // IMPORTS
+require('dotenv').config();
 const path = require('path');
+const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+const DB_URI = process.env.DB_URI;
 
 // MIDDLEWARE
-// Static files. Must be an public/index.html file to see response
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());                                            // CORS Support
+app.use(express.static(path.join(__dirname, 'public')));    // Static files. Must be an public/index.html file to see response
+
 
 app.use(express.json()); // JSON Support
 app.use(morgan('dev'));  // Logger
 
 // SERVER START
-app.listen(3000, () => console.log('Server started at port 3000'));
+app.listen(PORT, () => console.log('Server started at port ' + PORT));
