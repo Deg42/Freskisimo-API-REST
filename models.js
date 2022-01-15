45b3336159464714f6c30c6d386e338ac1522790
Juan Antonio González <jgonzalezrivera@eci.safanet.es>
2022-01-15 21:14:01 +0100

    added VEHICULO and REPARTIDOR schemes

--- models.js
+++ models.js
@@ -14,6 +14,28 @@ const Congelado = mongoose.model(
   )
 )
 
+// ~ VEHICULOS ~
+const Vehiculo = mongoose.model(
+  'Vehiculo',
+  new mongoose.Schema(
+    { matricula: { type: String, unique: true }, marca: String, modelo: String, anyo: Date},
+    {
+      versionKey: false,  // Ignore version key
+    }
+  )
+)
+
+// ~ REPARTIDORES ~
+const Repartido = mongoose.model(
+  'Repartidor',
+  new mongoose.Schema(
+    { dni : { type: String, unique: true }, nombre: String, apellido: String, repartiendo: Boolean, vehiculo: Vehiculo},
+    {
+      versionKey: false,  // Ignore version key
+    }
+  )
+)
+
 // EXPORTS
 module.exports = {
   Congelado: Congelado
